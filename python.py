53f976dde16e1d9fc6bda67b11261311b0b01a64
laylo <alaa.essa@ejust.edu.eg>
2024-08-13 13:10:28 +0300

    python file

---
+++ python.py
@@ -0,0 +1,13 @@
+def calculate_average(num1, num2, num3):
+    total = num1 + num2 + num3
+    average = total / 3
+    return average
+
+# Get input from the user
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+number3 = float(input("Enter the third number: "))
+# Calculate the average
+average_result = calculate_average(number1, number2, number3)
+print(f"The average of {number1}, {number2}, and {number3} is {average_result}")number3)
+print(f"The average of {number1}, {number2}, and {number3} is {average_result}")
